e8f36a103798b52c68673941717ceaf09425d45c
SuperSirBird <40967626+SuperSirBird@users.noreply.github.com>
2018-07-18 08:48:15 +1000

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,12 +1,11 @@
 // 3.0 Theme by @RAYTRACING WIP
 
-
 var defs = document.getElementsByClassName("blocklySvg")[0].children[0].innerHTML;
 
-var filter = " \
+var filter1 = " \
     <filter id='blockfilter' filterUnits='objectBoundingBox' x='-10%' y='-10%' width='150%' height='150%'> \
-		<feGaussianBlur in='SourceAlpha' stdDeviation='0.5' result='blur'/> \
-		<feSpecularLighting in='blur' surfaceScale='3' specularConstant='2.2' specularExponent='45' result='specOut' lighting-color='white'> \
+		<feGaussianBlur in='SourceAlpha' stdDeviation='0.9' result='blur'/> \
+		<feSpecularLighting in='blur' surfaceScale='3' specularConstant='2' specularExponent='50' result='specOut' lighting-color='white'> \
 			<fePointLight x='-5000' y='-5000' z='8000'/> \
 		</feSpecularLighting> \
 		<feComposite in='specOut' in2='SourceAlpha' operator='in' result='specOut2'/> \
@@ -17,8 +16,9 @@ var filter = " \
               0 0.85 0 0 0 \
               0 0 0.85 0 0 \
               0 0 0 1 0 '/> \
-	</filter>"
+	</filter>";
 
-document.getElementsByClassName("blocklySvg")[0].children[0].innerHTML = defs + filter;
+document.getElementsByClassName("blocklySvg")[0].children[0].innerHTML = defs + filter1;
 document.getElementsByClassName("blocklyBlockCanvas")[0].setAttribute("filter", "url(#blockfilter)");
+document.getElementsByClassName("blocklyBlockCanvas")[1].setAttribute("filter", "url(#blockfilter)");
 
