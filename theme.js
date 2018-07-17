5e1777d657deebcd1041dccdb8442f6a83fcee02
SuperSirBird <40967626+SuperSirBird@users.noreply.github.com>
2018-07-17 20:03:59 +1000

    Update theme.js

--- theme.js
+++ theme.js
@@ -4,11 +4,21 @@
 
 var defs = document.getElementsByClassName("blocklySvg")[0].children[0].innerHTML;
 
-var filter = "<filter id='blockfilter' x='0' y='0' width='300%' height='300%'> \
-                <feOffset result='offOut' in='SourceAlpha' dx='5' dy='5'></feOffset> \
-                <feGaussianBlur result='blurOut' in='offOut' stdDeviation='3'></feGaussianBlur> \
-                <feBlend in='SourceGraphic' in2='blurOut' mode='normal'></feBlend> \
-              </filter>"
+var filter = " \
+    <filter id='blockfilter' filterUnits='objectBoundingBox' x='-10%' y='-10%' width='150%' height='150%'> \
+		<feGaussianBlur in='SourceAlpha' stdDeviation='0.5' result='blur'/> \
+		<feSpecularLighting in='blur' surfaceScale='4' specularConstant='0.5' specularExponent='34' result='specOut' lighting-color='white'> \
+			<fePointLight x='-5000' y='-5000' z='8000'/> \
+		</feSpecularLighting> \
+		<feComposite in='specOut' in2='SourceAlpha' operator='in' result='specOut2'/> \
+		<feComposite in='SourceGraphic' in2='specOut2' operator='arithmetic' k1='0' k2='1' k3='1' k4='0' result='litPaint' /> \
+<feColorMatrix \
+      type='matrix' \
+      values='0.85 0 0 0 0 \
+              0 0.85 0 0 0 \
+              0 0 0.85 0 0 \
+              0 0 0 1 0 '/> \
+	</filter>"
 
 document.getElementsByClassName("blocklySvg")[0].children[0].innerHTML = defs + filter;
 
